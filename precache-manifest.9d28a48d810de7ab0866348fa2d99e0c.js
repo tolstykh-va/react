5725f1527aab748776fab11841c9b34c5586795e
Vladimir Tolstykh <mr.tolstykh.va@gmail.com>
2020-05-05 19:37:59 +0300

    add react app

---
+++ precache-manifest.9d28a48d810de7ab0866348fa2d99e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108e61e0cfa3ca1b7337a121ff85b555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e0b2728bcc2a1429c57",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "519feb113464b272d05c",
+    "url": "/static/js/2.14480b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14480b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0b2728bcc2a1429c57",
+    "url": "/static/js/main.545b9d72.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
